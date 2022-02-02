df1165287defda17e82e6189eadbe778b4bb08c3
taetae <peacepiece7@naver.com>
2022-02-02 13:36:55 +0900

    add : biz-adv, giz-simple

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,50 @@ const router = express.Router();
 
 const sectionIcons = ['🍚', '🍿', '🍜', '🍣', '🥩', '☕', '🍰'];
 
+const statusKorMap = {
+  OPN: '영업중',
+  CLS: '폐업',
+  VCT: '휴가중',
+  RMD: '리모델링',
+};
+
 router.get('/', async function (req, res, next) {
   const sections = await sql.getSections();
   sections.map((item) => {
     item.icon = sectionIcons[item.section_id - 1];
   });
-
   res.render('sections', {
     title: '섹션 목록',
     sections,
+  });
+});
+
+router.get('/biz-simple', async function (req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query);
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status];
+    return item;
+  });
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status];
+    return item;
+  });
+  res.render('biz-simple', {
+    title: '단순 식당 목록',
+    businesses,
+  });
+});
+
+router.get('/biz-adv', async function (req, res, next) {
+  const businesses = await sql.getBusinessesJoined(req.query);
+  businesses.map((item) => {
+    item.status_kor = statusKorMap[item.status];
+    return item;
+  });
+  res.render('biz-adv', {
+    title: '고급 식당 목록',
+    q: req.query,
+    businesses,
   });
 });
 
